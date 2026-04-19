3236ae4cd591d026d145e3b894678e8a9173f878
Marcin Olichwiruk <21108638+olichwiruk@users.noreply.github.com>
2026-04-19 15:42:44 +0200

    feat: add Sidekiq integration and environment-based DB configuration

--- config/application.rb
+++ config/application.rb
@@ -42,5 +42,10 @@ module ExpenseTracker
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.active_job.queue_adapter = :sidekiq
+
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_expense_tracker_session'
   end
 end
